9c3d3f8e5d02f24b8a38613c9842da7ce486468a
kasyap dharanikota <76957823+kasyap1234@users.noreply.github.com>
2021-05-08 19:57:58 +0530

    Create calculate_no_of_coins.py

---
+++ calculate_no_of_coins.py
@@ -0,0 +1,9 @@
+def count_coins(cents):
+    coins=[25,20,10,5,1]
+    count=0
+    for coin in coins:
+        while cents>=0:
+            cents=cents-coin
+            count+=1
+    return count
+print(count_coins(1000))
