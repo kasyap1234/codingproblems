a621ad20edbc0233388f3fc34a53b8bba6086c91
kasyap dharanikota <76957823+kasyap1234@users.noreply.github.com>
2021-05-22 20:00:01 +0530

    Update and rename Create a function that takes a list lst of numbers and moves all zeros to the end, preserving the order of the other elements. to rearrangearray.py

--- rearrangearray.py
+++ rearrangearray.py
@@ -1,4 +1,4 @@
-Create a function that takes a list lst of numbers and moves all zeros to the end, preserving the order of the other elements.
+#Create a function that takes a list lst of numbers and moves all zeros to the end, preserving the order of the other elements.
 
 def move_zeros(lst):
     newlst=[]
