92fddc318984defa62a97f5ea866c9908cee255c
kasyap dharanikota <76957823+kasyap1234@users.noreply.github.com>
2021-05-22 19:58:42 +0530

    Create all_subarrays_of_array.cpp

---
+++ all_subarrays_of_array.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<bits.h>
+using namespace std; 
+int main(){
+ // printing all the subarrays of a given array ; 
+ arr[5]={1,2,3,4,5}; 
+
+ for(int i=0;i<5;i++){
+     for(int j=i;j<5;j++){
+         for(int k=i;k<=j;k++{
+             cout << arr[k]; 
+
+         } 
+
+     }
+ }
+}
