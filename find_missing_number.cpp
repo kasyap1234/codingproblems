b15edc6fce12e803b8a50e3f1595f3fd3f1d3184
kasyap dharanikota <76957823+kasyap1234@users.noreply.github.com>
2021-06-13 14:24:53 +0530

    Create find_missing_number.cpp

---
+++ find_missing_number.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std; 
+void find(int arr[], int n){
+	int diff=arr[0]; 
+	for(int i=0; i<n; i++){
+		if (arr[i]-i!=diff){
+			cout<<i + diff<<endl;
+
+		}
+		break; 
+	}
+	void main(){
+		int  array[7]={1,2,3,4,5,6,8}; 
+		void find(*array,7); 
+	}
+
+	}
