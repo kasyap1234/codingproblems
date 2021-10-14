cbc3a8f365327ee48c31cfe51633dc6a7127eaf6
kasyap dharanikota <76957823+kasyap1234@users.noreply.github.com>
2021-10-14 17:48:01 +0530

    Create bst_from_preorder.cpp

---
+++ bst_from_preorder.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        if(preorder.size()==0){
+            return NULL; 
+            
+        }
+        
+        TreeNode*root=new TreeNode(preorder[0]); 
+       if(preorder.size()==1){
+           return root; 
+       }
+        vector<int>ltree; 
+        vector<int>rtree; 
+        for(int i=1;i<preorder.size();i++){
+            if(preorder[i]>preorder[0]){
+                rtree.push_back(preorder[i]); 
+            }
+            else{
+                ltree.push_back(preorder[i]); 
+                
+            }
+            root->left=bstFromPreorder(ltree); 
+            root->right=bstFromPreorder(rtree); 
+             
+            
+        }
+        return root; 
+        
+    }
+};
