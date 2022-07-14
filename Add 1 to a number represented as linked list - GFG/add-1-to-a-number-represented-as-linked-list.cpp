11094c3f3b56a1563d0824683041ccd8223767c5
kasyap dharanikota <76957823+kasyap1234@users.noreply.github.com>
2022-07-14 18:32:46 +0530

    Added solution - LeetHub

---
+++ Add 1 to a number represented as linked list - GFG/add-1-to-a-number-represented-as-linked-list.cpp
@@ -0,0 +1,150 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data; 
+        node = node->next; 
+    }  
+    cout<<"\n";
+} 
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+/* 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution
+{
+    private:
+      private:
+    struct Node* reverse(struct Node* head){
+         Node* curr = head;
+         Node* pre = NULL;
+         Node* next = NULL;
+        
+        while(curr != NULL){
+            next = curr->next;
+            curr->next = pre;
+            pre = curr;
+            curr = next;
+        }
+        return pre;
+    }
+    
+    void insertAtTail(struct Node* &head, struct Node* &tail, int d){
+         Node* temp = new Node(d);
+        if(head == NULL){
+            head = temp;
+            tail = temp;
+            return;
+        }
+        else{
+            tail->next = temp;
+            tail = temp;
+        }
+    }
+    
+    struct Node* sum(struct Node* first, struct Node* second){
+        int carry = 0;
+         Node* ansHead = NULL;
+         Node* ansTail = NULL;
+        
+        while(first != NULL || second != NULL ||  carry !=0){
+            int val1 = 0;
+            if(first != NULL)
+            val1 = first->data;
+            
+            int val2 = 0;
+            if(second != NULL)
+            val2 = second->data;
+            
+            int add = val1 + val2 +carry;
+            int digit = add%10;
+            insertAtTail(ansHead, ansTail, digit);
+            carry = add/10;
+            
+            if(first != NULL)
+            first = first->next;
+            
+            if(second != NULL)
+            second = second->next;
+        }
+        return ansHead;
+    }
+    
+    
+    public:
+    Node* addOne(Node *head) 
+    {
+        
+        Node* first = head;
+        Node* second = new Node(1);
+
+        first = reverse(first);
+        second = reverse(second);
+        
+         Node* ans = sum(first, second);
+        
+        ans = reverse(ans);
+        
+        return ans;
+
+
+    }
+};
+
+// { Driver Code Starts.
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        
+        Node* head = new Node( s[0]-'0' );
+        Node* tail = head;
+        for(int i=1; i<s.size(); i++)
+        {
+            tail->next = new Node( s[i]-'0' );
+            tail = tail->next;
+        }
+        Solution ob;
+        head = ob.addOne(head);
+        printList(head); 
+    }
+    return 0; 
+} 
+  // } Driver Code Ends
