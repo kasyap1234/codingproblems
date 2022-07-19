63cf42b9a300477dd8f874b9a96318ca3c4c2946
kasyap dharanikota <76957823+kasyap1234@users.noreply.github.com>
2022-07-19 11:17:01 +0530

    Create solution.cpp

---
+++ next_greater_element/solution.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the next greater element for each element of the array.
+    vector<long long> nextLargerElement(vector<long long> arr, int n){
+        // Your code here
+        stack<long long > st; 
+        vector<long long > ans; 
+        long long max; 
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && arr[i]>st.top()){
+                st.pop(); 
+            }
+            if(st.empty()){
+                ans.push_back(-1); 
+            }
+            else{
+                ans.push_back(st.top()); 
+            }
+            st.push(arr[i]); 
+            
+        }
+        reverse(ans.begin(),ans.end()); 
+        
+        return ans; 
+        
+    }
+};
+
+//{ Driver Code Starts.
+
